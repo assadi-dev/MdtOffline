0c8ce947bc01f885d1a4e63cfe1a2faca8ba700a
Assadi Halifa <assadi.halifa@gmail.com>
2022-09-14 21:53:22 +0200

    Update Api.js

--- assets/service/Api/Api.js
+++ assets/service/Api/Api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const getTokenStorage = localStorage.getItem("mdtOfflineToken-999");
 
 const init = axios.create({
-  baseURL: "http://localhost:8000/api",
+  baseURL: "http://176.97.210.240/api",
   headers: {
     "Content-type": "application/json",
   },
