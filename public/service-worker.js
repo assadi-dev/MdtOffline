0f7dc7db8df83a311b779632b2adaaeb55e2d90f
Assadi Halifa <assadi.halifa@gmail.com>
2022-10-15 15:31:31 +0200

    getion des caches de puis le service workers

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,11 +1,6 @@
 const CACHE_NAME = "MDT-Offline-static-v1";
 
-const app_shell_files = [
-  "./manifest.json",
-  "./index.php",
-  "./build/",
-  "./favicon.ico",
-];
+const app_shell_files = ["./index.php", "./build/", "./.htaccess"];
 
 self.addEventListener("install", (event) => {
   console.log("new Service Worker installé !");
@@ -20,19 +15,33 @@ self.addEventListener("install", (event) => {
 });
 
 self.addEventListener("activate", () => {
-  console.log("Service Worker activé !");
+  //console.log("Service Worker activé !");
   self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((resp) => {
+      return (
+        resp ||
+        fetch(event.request).then((response) => {
+          return caches.open(CACHE_NAME).then((cache) => {
+            cache.put(event.request, response.clone());
+            return response;
+          });
+        })
+      );
+    })
+  );
 });
 
 //Supprimer les caches
 self.addEventListener("activate", (e) => {
   e.waitUntil(
     caches.keys().then((keys) => {
-      return new Promise.add(
+      return Promise.all(
         keys.filter((key) => key != CACHE_NAME).map((key) => caches.delete(key))
       );
     })
   );
 });
-
-self.addEventListener("fetch", (event) => {});
