125990284ba38845f10753546e58b7aec53eccf3
Assadi Halifa <assadi.halifa@gmail.com>
2022-10-15 13:43:45 +0200

    Config Service Worker + Ajouts du site en HTTPS

--- public/service-worker.js
+++ public/service-worker.js
@@ -3,39 +3,36 @@ const CACHE_NAME = "MDT-Offline-static-v1";
 const app_shell_files = [
   "./manifest.json",
   "./index.php",
-  "./build",
+  "./build/",
   "./favicon.ico",
 ];
 
-if ("serviceWorker" in navigator) {
-  self.addEventListener("install", (event) => {
-    console.log("new Service Worker installé !");
-    //A l'installation ajoute les fichiers en cache, puis active le Service Worker
+self.addEventListener("install", (event) => {
+  console.log("new Service Worker installé !");
+  //A l'installation ajoute les fichiers en cache, puis active le Service Worker
 
-    event.waitUntil(
-      caches.open(CACHE_NAME).then((cache) => cache.addAll(app_shell_files))
-    );
-  });
+  event.waitUntil(
+    caches
+      .open(CACHE_NAME)
+      .then((cache) => cache.addAll(app_shell_files))
+      .then(() => self.skipWaiting())
+  );
+});
 
-  self.addEventListener("fetch", (event) => {
-    console.log(event.request);
-  });
+self.addEventListener("activate", () => {
+  console.log("Service Worker activé !");
+  self.clients.claim();
+});
 
-  self.addEventListener("activate", () => {
-    console.log("Service Worker activé !");
-    self.clients.claim();
-  });
+//Supprimer les caches
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => {
+      return new Promise.add(
+        keys.filter((key) => key != CACHE_NAME).map((key) => caches.delete(key))
+      );
+    })
+  );
+});
 
-  //Supprimer les caches
-  self.addEventListener("activate", (e) => {
-    e.waitUntil(
-      caches.keys().then((keys) => {
-        return new Promise.add(
-          keys
-            .filter((key) => key != CACHE_NAME)
-            .map((key) => caches.delete(key))
-        );
-      })
-    );
-  });
-}
+self.addEventListener("fetch", (event) => {});
