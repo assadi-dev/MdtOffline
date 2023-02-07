14df9b148720a9bfe47315c2eef55f118cade373
Assadi <62776480+assadi-dev@users.noreply.github.com>
2023-02-07 02:01:38 +0100

    delete dossierArrestation

--- assets/Pages/Civil/Selected/ListItemViews/DeleteView/DeleteDossierArrestationView.jsx
+++ assets/Pages/Civil/Selected/ListItemViews/DeleteView/DeleteDossierArrestationView.jsx
@@ -37,7 +37,7 @@ const DeleteDossierArrestationView = ({ id, onClose, civil }) => {
   const handleSubmit = (e) => {
     e.preventDefault();
 
-    dispatch(deleteDossierArrestationAsync(id)).then(() => {
+    dispatch(deleteDossierArrestationAsync({ id })).then(() => {
       const { id } = civil;
       dispatch(getOneCivilsAsync({ id }));
       onClose();
