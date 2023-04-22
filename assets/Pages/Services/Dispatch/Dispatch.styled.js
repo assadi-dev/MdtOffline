2c5f4839265d8a192e61f16d2068f0654eaf78e8
Assadi <62776480+assadi-dev@users.noreply.github.com>
2023-04-22 20:25:55 +0200

    Update Dispatch.styled.js

--- assets/Pages/Services/Dispatch/Dispatch.styled.js
+++ assets/Pages/Services/Dispatch/Dispatch.styled.js
@@ -26,7 +26,7 @@ export const HeaderSection = styled.div`
   margin-bottom: 3.5rem;
   .title {
     @media screen and (min-width: 992px) {
-      font-size: 3rem;
+      font-size: 2.4rem;
       letter-spacing: 2px;
     }
   }
