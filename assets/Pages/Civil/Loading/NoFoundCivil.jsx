74f1785a3a392f60df60952355f5bb865a230729
Assadi <62776480+assadi-dev@users.noreply.github.com>
2023-05-21 18:48:27 +0200

    Update NoFoundCivil.jsx

--- assets/Pages/Civil/Loading/NoFoundCivil.jsx
+++ assets/Pages/Civil/Loading/NoFoundCivil.jsx
@@ -4,7 +4,7 @@ import { NoFoundCivilSearchContainer } from "./LoadingCivilList.style";
 export const NoFoundCivil = () => {
   return (
     <NoFoundCivilSearchContainer>
-      <p>Aucun résultat contenant le terme recherche n'a été trouvé</p>
+      <p>Aucun résultat contenant le terme recherché n'a été trouvé</p>
     </NoFoundCivilSearchContainer>
   );
 };
